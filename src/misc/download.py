48e80001fa4d3af0c3ec8e5ca43a559d6516f98c
Enakin Skywalker <levon12341@gmail.com>
2025-10-20 01:14:17 +0300

    download.py: changed links for updated conformer/3-gram models

--- src/misc/download.py
+++ src/misc/download.py
@@ -7,9 +7,9 @@ parser.add_argument('run_name')
 
 file_to_url = {
     'conformer-train-clean-100-beam-search-3-gram.zip': 'https://drive.google.com/file/d/1DikeH61alDP5wu0AfDDkiDmc98xXtjxo/view?usp=sharing',
-    'conformer-train-other-beam-search-3-gram-continue.zip': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
+    'conformer-train-other-beam-search-3-gram-continue.zip': 'https://drive.google.com/file/d/1PnLA3aWL1r_N-dKapqL0N1F1S1XJxLW4/view?usp=drive_link',
     'conformer-train-other-beam-search-3-gram.zip': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
-    'librispeech-3-gram.zip': 'https://drive.google.com/file/d/1BrvswKjl_3WncPI8ClhGkXf0IYorLlLg/view?usp=drive_link',
+    'librispeech-3-gram.zip': 'https://drive.google.com/file/d/1s7OMLJ4Q1st-U3-3k5zU2JeAS75UvJZV/view?usp=drive_link',
 }
 
 
