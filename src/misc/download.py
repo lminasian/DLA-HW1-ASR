399760c1c7492a7b1a73668956d5401a8b0343dd
Enakin Skywalker <levon12341@gmail.com>
2025-10-19 23:32:47 +0300

    Added logs of tuning lm weight

--- src/misc/download.py
+++ src/misc/download.py
@@ -6,17 +6,15 @@ parser.add_argument('run_name')
 
 
 file_to_url = {
-    'conformer-train-clean-100-beam-search-3-gram': 'https://drive.google.com/file/d/1DikeH61alDP5wu0AfDDkiDmc98xXtjxo/view?usp=sharing',
-    'conformer-train-other-beam-search-3-gram-continue': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
-    'conformer-train-other-beam-search-3-gram': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
-    'librispeech-3-gram': 'https://drive.google.com/file/d/1BrvswKjl_3WncPI8ClhGkXf0IYorLlLg/view?usp=drive_link',
+    'conformer-train-clean-100-beam-search-3-gram.zip': 'https://drive.google.com/file/d/1DikeH61alDP5wu0AfDDkiDmc98xXtjxo/view?usp=sharing',
+    'conformer-train-other-beam-search-3-gram-continue.zip': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
+    'conformer-train-other-beam-search-3-gram.zip': 'https://drive.google.com/file/d/1HjbCC1rymKlkB0kCUPCe-_77kKDZpIvI/view?usp=drive_link',
+    'librispeech-3-gram.zip': 'https://drive.google.com/file/d/1BrvswKjl_3WncPI8ClhGkXf0IYorLlLg/view?usp=drive_link',
 }
 
 
-checkpoint = 'conformer-train-clean-100-beam-search-3-gram-continue.zip' # final model
-gdown.download(file_to_url[checkpoint], checkpoint)
+checkpoint = 'conformer-train-other-beam-search-3-gram-continue.zip' # final model
+gdown.download(file_to_url[checkpoint], checkpoint, fuzzy=True)
 
 lm = 'librispeech-3-gram.zip'
-gdown.download(file_to_url[lm], lm)
-
-
+gdown.download(file_to_url[lm], lm, fuzzy=True)
